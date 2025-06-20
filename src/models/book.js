285c80ce7a1233291c6ee17983cbfa1f6e59ff12
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-06-20 21:17:52 +0530

    Added the book model to store the book schema

---
+++ src/models/book.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    condition: {
+        type: String,
+        enum: ["New", "like-new", "good", "fair", "poor"]
+    },
+    status: {
+        type: String,
+        enum: ["Available", "pending", "swapped"],
+        default: "available"
+    },
+    listedBy: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model("Book",bookSchema)
