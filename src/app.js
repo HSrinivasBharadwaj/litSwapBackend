25eea2c2f84ec40c6668cee5ca06059ec5aab50d
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-06-20 08:43:58 +0530

    Added the profile Router

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./config/database");
 const authRouter = require("./routes/auth");
+const profileRouter = require('./routes/profile');
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser())
 
-app.use("/",authRouter)
+app.use("/",authRouter);
+app.use("/",profileRouter)
 
 connectToDb()
   .then(() => {
