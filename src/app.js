0d2f6fb5f0386af6c19cb8f8b5b71d2432fd2078
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-06-21 11:10:28 +0530

    Added the bookRouter

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const connectToDb = require("./config/database");
 const authRouter = require("./routes/auth");
 const profileRouter = require('./routes/profile');
+const bookRouter = require('./routes/bookList');
 const port = process.env.PORT;
 
 app.use(express.json());
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(cookieParser())
 
 app.use("/",authRouter);
-app.use("/",profileRouter)
+app.use("/",profileRouter);
+app.use("/",bookRouter)
 
 connectToDb()
   .then(() => {
